02d75b95b91ba824c5eadacb005096551ef60caf
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-02-23 01:37:46 +0700

    feat: add employee (#6)
    * feat and refactor: home page, add filter according booking status
    
    * feat: add employee

--- src/components/employee/employeeTable.jsx
+++ src/components/employee/employeeTable.jsx
@@ -4,16 +4,25 @@ import { DataTable } from "primereact/datatable";
 import { useState } from "react";
 import { columns } from "~/const/employee";
 import EmployeeDetail from "./employeeDetail/employeeDetail";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { InputText } from "primereact/inputtext";
 
-export default function EmployeeTable({ listEmployees, handleListEmployee }) {
+export default function EmployeeTable({
+  listEmployees,
+  handleListEmployee,
+  searchInput,
+  setSearchInput,
+  setIsOpenAddDialog,
+}) {
   const [isOpenDialog, setIsOpenDialog] = useState(false);
   const [detailEmployee, setDetailEmployee] = useState(null);
 
   const actionTemplate = (rowData) => (
     <div className="flex gap-2">
       <Button
-        label="Detail"
-        severity="warning"
+        label={<i className="pi pi-eye"></i>}
+        severity="secondary"
         outlined
         raised
         onClick={() => {
@@ -21,35 +30,45 @@ export default function EmployeeTable({ listEmployees, handleListEmployee }) {
           setDetailEmployee(rowData);
         }}
       />
-      <Button label="Delete" severity="danger" outlined raised />
     </div>
   );
 
   return (
-    <div>
+    <div className="border rounded-xl p-4">
       <EmployeeDetail
         isOpenDialog={isOpenDialog}
         setIsOpenDialog={setIsOpenDialog}
         detailEmployee={detailEmployee}
         handleListEmployee={handleListEmployee}
       />
+      <div className="flex justify-between items-center mb-4">
+        <p className="font-medium">Employee Management</p>
+        <div className="flex gap-4">
+          <IconField iconPosition="left">
+            <InputIcon className="pi pi-search"> </InputIcon>
+            <InputText
+              placeholder="Search"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className="w-[200px]"
+            />
+          </IconField>
+
+          <Button
+            severity="success"
+            className="flex justify-between items-center w-[90px]"
+            onClick={() => setIsOpenAddDialog(true)}
+          >
+            <i className="pi pi-plus-circle" />
+            <p>Add</p>
+          </Button>
+        </div>
+      </div>
       <DataTable
         value={listEmployees}
         stripedRows
         style={{ minWidth: "700px" }}
         tableStyle={{ minWidth: "50rem" }}
-        header={
-          <div className="flex justify-between items-center">
-            <p>Employee Management</p>
-            <Button
-              severity="success"
-              className="flex justify-between items-center w-[90px]"
-            >
-              <i className="pi pi-plus-circle" />
-              <p>Add</p>
-            </Button>
-          </div>
-        }
       >
         {columns.map((col) => (
           <Column
@@ -62,7 +81,7 @@ export default function EmployeeTable({ listEmployees, handleListEmployee }) {
         <Column
           field="action"
           header="Action"
-          style={{ width: "150px" }}
+          style={{ width: "80px" }}
           body={actionTemplate}
         />
       </DataTable>
